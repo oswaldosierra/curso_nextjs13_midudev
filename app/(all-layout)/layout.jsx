891776891a76c4be8f3e63ac0467c469b7d3f928
Oswaldo Luis Sierra Bran <oswaldox199@gmail.com>
2023-08-10 21:59:38 -0500

    Se agrega layout a 2 rutas sin estar en el mismo arbol

---
+++ app/(all-layout)/layout.jsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+    return (
+        <div>
+            <marquee behavior="" direction="">hola que tal soy un layout</marquee>
+            {children}
+        </div>
+    )
+}
