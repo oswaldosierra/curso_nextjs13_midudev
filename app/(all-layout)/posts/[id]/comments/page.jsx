71778449c5ad1071be2863658bade5a2597f5681
Oswaldo Luis Sierra Bran <oswaldox199@gmail.com>
2023-08-11 10:58:38 -0500

    Se termina curso de nextjs 13

--- app/(all-layout)/posts/[id]/comments/page.jsx
+++ app/(all-layout)/posts/[id]/comments/page.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 const fetchComments = (id) => {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, { cache: 'no-store' })
@@ -12,6 +13,7 @@ export default async function Post({ params }) {
         <ul>
             {comments.map(comment => (
                 <li className="border shadow-md my-5 p-3" key={comment.id}>
+                    <Image src={`https://api.dicebear.com/6.x/adventurer/svg?seed=${comment.email}`} alt="avatar" width={40} height={40} />
                     <h2 className="font-bold">📎{comment.name}</h2>
                     <p>{comment.body}</p>
                 </li>
